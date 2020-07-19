ecc54d38f52ea176b558283878294db0b2e536de
Joachim Junior <joachimjunior5@gmail.com>
2020-07-19 23:11:52 +0100

    Last update

--- js/main.js
+++ js/main.js
@@ -3,4 +3,18 @@ $(document).ready(function() {
     SyntaxHighlighter.defaults['toolbar'] = false;
     SyntaxHighlighter.all();
 
+    $('#admin-switch').on('click', function () 
+    {
+        console.log('kdddsd');
+        $('#user-section').css('cssText', 'display: none');
+        $('#admin-section').css('cssText', 'display: block !important;');
+    });
+
+    $('#user-switch').on('click', function () 
+    {
+        console.log('kdddsd');
+        $('#user-section').css('cssText', 'display: block !important');
+        $('#admin-section').css('cssText', 'display: none !important;');
+    });
+
 });
